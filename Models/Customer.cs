d0df0e2d36f7109476018ddd53fb8c564ff213d3
andres3m <andresmartinezuk@gmail.com>
2024-10-11 11:09:39 +0100

    Fix set up files

---
+++ Models/Customer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace customers_inventory.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime RegistrationDate { get; set; }
+        public string? Gender { get; set; }
+        public decimal Age { get; set; }
+    }
+}
---
+++ Pages/Customers/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using customers_inventory.Data;
+using customers_inventory.Models;
+
+namespace customers_inventory.Pages.Customers
+{
+    public class IndexModel : PageModel
+    {
+        private readonly customers_inventory.Data.customers_inventoryContext _context;
+
+        public IndexModel(customers_inventory.Data.customers_inventoryContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Customer> Customer { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Customer = await _context.Customer.ToListAsync();
+        }
+    }
+}
