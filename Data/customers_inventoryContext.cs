c1500171b09c951ca12c0fac6bd09185773c8969
andres3m <andresmartinezuk@gmail.com>
2024-10-11 12:12:47 +0100

    Pages update

--- Data/customers_inventoryContext.cs
+++ Data/customers_inventoryContext.cs
@@ -1,19 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using customers_inventory.Models;
 
 namespace customers_inventory.Data
 {
     public class customers_inventoryContext : DbContext
     {
-        public customers_inventoryContext (DbContextOptions<customers_inventoryContext> options)
+        public customers_inventoryContext(DbContextOptions<customers_inventoryContext> options)
             : base(options)
         {
         }
 
-        public DbSet<customers_inventory.Models.Customer> Customer { get; set; } = default!;
+        public DbSet<Customer> Customer { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                // Retrieve connection string from the environment variable
+                var connectionString = Environment.GetEnvironmentVariable("DB_CONNECTION_STRING");
+
+                if (!string.IsNullOrEmpty(connectionString))
+                {
+                    optionsBuilder.UseNpgsql(connectionString);
+                }
+                else
+                {
+                    throw new InvalidOperationException("The connection string was not found in environment variables.");
+                }
+            }
+        }
     }
 }
